b6f94451bf5a25bbf96321244791384b6522347a
Uti-Favour <utifavour15@gmail.com>
2024-03-12 20:37:33 +0100

    Made the Nav Bar responsive when clicked

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,12 @@
 document.addEventListener('DOMContentLoaded' , () => {
+const navBar = document.getElementById('navBar');
+let nav_hidden = document.querySelector('.nav_hidden');
 
+navBar.addEventListener('click' , () => {
+nav_hidden.classList.toggle('nav_display');
+nav_hidden.classList.toggle('nav_hidden');
+
+});
 
 
 
