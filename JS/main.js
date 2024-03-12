86f7819102ffb1a3294337ec59d385d0e735a236
Uti-Favour <utifavour15@gmail.com>
2024-03-12 21:06:31 +0100

    FAQ section

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,9 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const navBar = document.getElementById('navBar');
-    let nav_hidden = document.querySelector('.nav_hidden');
+document.addEventListener("DOMContentLoaded", () => {
+  const navBar = document.getElementById("navBar");
+  let nav_hidden = document.querySelector(".nav_hidden");
 
-    navBar.addEventListener('click', () => {
-        nav_hidden.classList.toggle('nav_display');
-        nav_hidden.classList.toggle('nav_hidden');
-
-    });
-
-
+  navBar.addEventListener("click", () => {
+    nav_hidden.classList.toggle("nav_display");
+    nav_hidden.classList.toggle("nav_hidden");
+  });
 });
