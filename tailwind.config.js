62265dbe04e46ecd534b9d37f08617e676c61cb7
Uti-Favour <utifavour15@gmail.com>
2024-03-12 16:32:52 +0100

    Hero Section design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
-  theme: {
-    extend: {},
-  },
   plugins: [],
+
+  theme: {
+    fontFamily: {
+      brandFont: ["'YourFontName', sans-serif"],
+    },
+    // ... other Tailwind configurations
+  }
 }
 
