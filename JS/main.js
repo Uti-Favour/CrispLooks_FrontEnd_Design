092e935718f7541afb536ef7b17f5cb77650a4da
Uti-Favour <utifavour15@gmail.com>
2024-03-14 14:53:04 +0100

    Added the products in a Object using JS

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,88 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navBar = document.getElementById("navBar");
   let nav_hidden = document.querySelector(".nav_hidden");
+  const items = document.getElementById("items");
 
   navBar.addEventListener("click", () => {
     nav_hidden.classList.toggle("nav_display");
     nav_hidden.classList.toggle("nav_hidden");
   });
+
+  // ------ Cloth List Items -------
+  const orderList = [
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    {
+      src: "Images/collection-2.webp",
+      title: "URBAN MEN ANKARA",
+      description: "VINTAGE ANKARA STYLES",
+      price: "$10,000",
+    },
+
+    
+  ];
+
+  console.log(orderList.description(1));
 });
