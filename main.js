cc38839a3e6c51c3999f7a97c650bba812e8fb5f
Uti-Favour <utifavour15@gmail.com>
2024-03-26 17:24:25 +0100

    javascript changes

--- main.js
+++ main.js
@@ -82,4 +82,33 @@ document.addEventListener("DOMContentLoaded", () => {
     },
   ];
 
+
+  orderList.forEach ( element => {
+
+    function createProductElement(){
+
+
+
+      return
+`
+<div class="grid lg:grid-cols-2 lg:mx-10 mx-5 gap-5">
+            <div class="relative">
+                <img src="assets/collection-15.jpg" class="rounded-lg lg:rounded-2xl" />
+                <div
+                    class="absolute bottom-0 right-0 bg-white text-black rounded-br-md lg:rounded-br-2xl rounded-tl-2xl px-10 lg:px-16 py-2 lg:py-5 max-w-sm">
+                    <div>
+                        <span class="font-bold">${`orderList.description`}</span>
+                    </div>
+                    <div class="mt-5">
+                        <span class="py-2 px-10 bg-black text-white rounded-2xl mt-5">${`orderList.price`} <i
+                                class="fas fa-arrow-right ml-2"></i></span>
+                    </div>
+                </div>
+            </div>
+`
+    };
+
+
+  });
+
 });
